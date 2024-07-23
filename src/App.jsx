5921ad67789196fa543a97c59a09abecd80678b1
mokamalelidn <mhmdalknj496@gmail.com>
2024-07-23 12:28:00 +0300

    chore: Update header and main content in index.html and App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,23 @@ function App() {
   
 
   return (
-    <h1>React Tic-Tac-Toe</h1>
+    <main>
+      <div id="game-container">
+        <ol id="players">
+          <li >
+            <span className="player-name">Player 1</span>
+            <span className="player-symbol">x</span>
+          </li>
+          <li >
+            <span className="player-name">Player 2</span>
+            <span className="player-symbol">o</span>
+          </li>
+        </ol>
+
+        Game
+      </div>
+      log
+    </main>
   )
 }
 
